52b3c07f82923cfad1da636f1ff2316a848d029c
Jason van Zyl <jason@vanzyl.ca>
2020-03-28 08:44:47 -0400

    Move OkHttpDownloadManager to its own class

--- src/main/java/com/walmartlabs/concord/plugins/ConcordTestSupport.java
+++ src/main/java/com/walmartlabs/concord/plugins/ConcordTestSupport.java
@@ -11,23 +11,15 @@ import com.walmartlabs.concord.sdk.ImmutableBucketInfo;
 import com.walmartlabs.concord.sdk.LockService;
 import com.walmartlabs.concord.sdk.ObjectStorage;
 import com.walmartlabs.concord.sdk.SecretService;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.junit.Before;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.Reader;
 import java.lang.reflect.Field;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -324,60 +316,6 @@ public abstract class ConcordTestSupport
         }
         catch (IOException e) {
             return null;
-        }
-    }
-
-    // ------------------------------------------------------------------------------------------------------
-    // DependencyManager
-    // ------------------------------------------------------------------------------------------------------
-
-    public static class OKHttpDownloadManager
-            implements DependencyManager
-    {
-
-        private final File toolDir;
-
-        public OKHttpDownloadManager(String tool)
-        {
-            this.toolDir = new File(System.getProperty("user.home"), ".m2/tools/" + tool);
-            this.toolDir.mkdirs();
-        }
-
-        @Override
-        public Path resolve(URI uri)
-                throws IOException
-        {
-            String urlString = uri.toString();
-            String fileName = urlString.substring(urlString.lastIndexOf('/') + 1);
-            File target = new File(toolDir, fileName);
-            if (!target.exists()) {
-                OkHttpClient client = new OkHttpClient();
-                Request request = new Request.Builder().url(urlString).build();
-                Call call = client.newCall(request);
-                Response response = call.execute();
-                if (response.code() == 404) {
-                    throw new RuntimeException(String.format("The URL %s doesn't exist.", uri));
-                }
-                download(response.body().byteStream(), target);
-            }
-            return target.toPath();
-        }
-
-        //
-        // https://stackoverflow.com/questions/309424/how-do-i-read-convert-an-inputstream-into-a-string-in-java
-        //
-        // surprised this is the fastest way to convert an inputstream to a string
-        //
-        private void download(InputStream stream, File target)
-                throws IOException
-        {
-            byte[] buffer = new byte[8192];
-            int length;
-            try (OutputStream result = new FileOutputStream(target)) {
-                while ((length = stream.read(buffer)) != -1) {
-                    result.write(buffer, 0, length);
-                }
-            }
         }
     }
 }
